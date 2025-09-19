231a1a72321835c6a80c2098ba5c4cb96e2a60c3
Mahdi <mahdi.mousavi0011@gmail.com>
2025-09-19 14:12:28 +0200

    delete my password from database.js

--- animal-db/config/database.js
+++ animal-db/config/database.js
@@ -10,7 +10,7 @@ export const getConnection = async () => {
             host: "localhost",
             port: 3306,
             user: "root",
-            password: "Sommar24",
+            password: "your password",
             database: "animals_db",
         });
     }
